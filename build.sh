cdb8feaaf1879a9711d4e759f1c388b40a5b137c
Pointless <me@pointless.me>
2017-09-07 13:49:13 +1200

    Made the email actually work

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ npm run build
 # build let's go have some fun with the cloned repo
 cd build
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "travis@pointless.me"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
